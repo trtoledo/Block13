4a9a23eb57e9d1ef0629d9176052987079df8314
Tomas Toledo <trtoledo1977@gmail.com>
2024-12-19 13:25:32 -0300

    isNaN conditional

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const age = prompt("Please enter your age")
+const age = parseInt(prompt("Please enter your age"))
 console.log(age);
+//let mustPay false;
 
-if (age <= 12){
-    // do this if customer is yunger than 12
+if (isNaN(age)){
+    alert("Please enter a valid number for age")
+}
+
+if (age <= 12 || age >= 65){
+    // do this if customer is yunger than 12 or older than 65
     alert("The customer ticket is free!");
 }
-else if (age >=65){
-    alert("Thge customer ticket is free!!");
-} else {
+//else if (age >=65){
+// alert("Thge customer ticket is free!!");} 
+   
+   else {
     alert("Plaese pay the full price");
+    mustPay = true
+}
+if (mustPay){
+    const amount =prompt("Please pay U$15");
+    if (amount == 15){
+        alert("Thank you! Come on in!");
+    }else {
+        alert("Sorry it`s exactly U$15");
+    }
 }
